bb1e689e82b2713bbaf24e07144395b36f213df2
Ransilu Ranaweera <ransihowler@gmail.com>
2025-04-13 11:38:59 +0930

    Step one - setup

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,6 @@
 package students.items;
 
-public class Item {
+public abstract class Item {
 	double age = 0;
 	double maturationAge;
 	double deathAge;
@@ -30,8 +30,31 @@ public class Item {
 		return false;
 	}
 	
+//get value for food items if fully grown
+	public double getValue() {
+	    if (this.age >= this.maturationAge) {
+	        return this.monetaryValue;
+	    }
+	    return 0;
+	}
 
+// check if any other object has equal values
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+	    if (!(obj instanceof Item)) return false;
+  
+	    Item other = (Item) obj;
+	    if (this.age != other.age) return false;
+	    if (this.maturationAge != other.maturationAge) return false;
+	    if (this.deathAge != other.deathAge) return false;
+	    if (this.monetaryValue != other.monetaryValue) return false;
+	    
+	    return true;
+	}
 	
-	
+//	abstract function for subclasses to return a string representation of the item
+	public abstract String toString();
+
 	
 }
